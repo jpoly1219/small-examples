943393102c39cae9c0ffdc545bb49ffa8bdbfb47
jpoly1219 <jpoly@umich.edu>
2024-12-26 20:07:58 -0500

    playing around with middleware

--- session-token/main.go
+++ session-token/main.go
@@ -27,6 +27,26 @@ func generateToken(length int) (string, error) {
 	return hex.EncodeToString(randomBytes), nil
 }
 
+type AuthMiddleware struct {
+	handler http.Handler
+}
+
+func (h *AuthMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("check auth")
+	authorized := checkAuthorization(r)
+	log.Printf("Authorized? %v", authorized)
+	if !authorized {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	h.handler.ServeHTTP(w, r)
+}
+
+func NewAuthMiddleware(next http.Handler) http.Handler {
+	return &AuthMiddleware{next}
+}
+
 type IndexHandler struct{}
 
 func (h *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +55,8 @@ func (h *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.Method == http.MethodPost && r.URL.Path == "/login":
 		h.login(w, r)
-	case r.Method == http.MethodPost && r.URL.Path == "/protected":
-		h.protected(w, r)
+	case r.Method == http.MethodGet && r.URL.Path == "/protected":
+		NewAuthMiddleware(http.HandlerFunc(h.protectedResource)).ServeHTTP(w, r)
 	default:
 		fmt.Fprintf(w, "called /")
 	}
@@ -44,6 +64,10 @@ func (h *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func handleCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+type LoginResponse struct {
+	Response string `json:"response"`
 }
 
 func (h *IndexHandler) login(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +85,7 @@ func (h *IndexHandler) login(w http.ResponseWriter, r *http.Request) {
 	d.DisallowUnknownFields()
 	err := d.Decode(&cred)
 	if err != nil {
-		log.Println("Failed to read credentials")
+		log.Printf("Failed to read credentials: %v", err)
 		http.Error(w, "Failed to read credentials", http.StatusInternalServerError)
 		return
 	}
@@ -97,7 +121,7 @@ func (h *IndexHandler) login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err != nil {
-		log.Println("Failed to query")
+		log.Printf("Failed to query")
 		http.Error(w, "Failed to query", http.StatusInternalServerError)
 		return
 	}
@@ -143,16 +167,72 @@ func (h *IndexHandler) login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.SetCookie(w, cookie)
-	w.Write([]byte("Authenticated!"))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(LoginResponse{"Authenticated!"})
 }
 
-func (h *IndexHandler) protected(w http.ResponseWriter, r *http.Request) {
-	handleCors(&w)
+func checkAuthorization(r *http.Request) bool {
+	// Check session token.
+	authHeader := r.Header.Get("Authentication")
+	if authHeader == "" {
+		log.Printf("Failed to read authentication header: Authentication header does not exist.")
+		return false
+	}
 
-	// check session token
-	// check in the database to see if this matches
+	splitAuthHeader := strings.Split(authHeader, "Bearer ")
+	if len(splitAuthHeader) == 1 && splitAuthHeader[0] == authHeader {
+		log.Printf("Failed to read authentication header: Authentication header does not include Bearer.")
+		return false
+	}
+
+	sessionToken := splitAuthHeader[1]
+	log.Printf("sessionToken: %v", sessionToken)
+
+	// Check in the database to see if this matches.
+	queryStr := "SELECT username FROM users WHERE session_token = $1;"
+	var username string
+	err := db.QueryRow(queryStr, sessionToken).Scan(&username)
+
+	// if no match, error out
+	if err == sql.ErrNoRows {
+		log.Println("Invalid credentials")
+		return false
+	}
+
+	if err != nil {
+		log.Printf("Failed to query: %v", err)
+		return false
+	}
+
 	// if match, check the matched user's role
+	queryStr = "SELECT role FROM users WHERE username = $1;"
+	var role string
+	err = db.QueryRow(queryStr, username).Scan(&role)
+	if err == sql.ErrNoRows {
+		log.Println("User has no roles assigned.")
+		return false
+	}
+
+	if err != nil {
+		log.Printf("Failed to query: %v", err)
+		return false
+	}
+
 	// check if the user can perform this action
+	if role == "user" {
+		return true
+	} else {
+		log.Println("Unauthorized")
+		return false
+	}
+}
+
+func (h *IndexHandler) protectedResource(w http.ResponseWriter, r *http.Request) {
+	type Response struct {
+		Message string `json:"message"`
+	}
+	json.NewEncoder(w).Encode(Response{"super secret"})
 }
 
 func main() {
@@ -174,7 +254,8 @@ func main() {
 		"CREATE TABLE IF NOT EXISTS users (",
 		"username TEXT PRIMARY KEY,",
 		"pw_hash TEXT UNIQUE NOT NULL,",
-		"session_token TEXT UNIQUE",
+		"session_token TEXT UNIQUE,",
+		"role TEXT",
 		")",
 	}, "")
 	_, err = db.Exec(queryStr)
@@ -183,11 +264,11 @@ func main() {
 	}
 
 	queryStr = strings.Join([]string{
-		`INSERT INTO users (username, pw_hash) VALUES ("user1", "$2a$10$WCdbhD2HDUsqO7b3bD/RwekiV8e/2xacF.anrualxypMS7WgpitWe")`,
+		`INSERT INTO users (username, pw_hash, role) VALUES ("user1", "$2a$10$WCdbhD2HDUsqO7b3bD/RwekiV8e/2xacF.anrualxypMS7WgpitWe", "user")`,
 	}, "")
 	_, err = db.Exec(queryStr)
 	if err != nil {
-		log.Fatal("Failed to insert user")
+		log.Fatalf("Failed to insert user, %v", err)
 	}
 
 	// pwHash, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
